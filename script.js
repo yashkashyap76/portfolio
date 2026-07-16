9504aa71cb37c3742606bd6b905369e0525ded48
yashkashyap76 <rishuraj123575@gmail.com>
2026-07-16 23:49:08 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// Typing Animation
+
+var typed = new Typed("#typing", {
+
+    strings: [
+        "Web Developer",
+        "Python Programmer",
+        "Frontend Developer",
+        "GitHub Creator"
+    ],
+
+    typeSpeed: 80,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+
+});
